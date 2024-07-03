1299cdee8e15e9cdb4e3611e5ccf3c2588c53759
eunah112 <shdmsdk2012@gmail.com>
2024-07-03 17:04:28 +0900

    nav 햄버거바 수정

--- js/header.js
+++ js/header.js
@@ -8,3 +8,28 @@ document.addEventListener('scroll', () => {
         menu.classList.remove('active');
     }
 });
+
+document.getElementById('hamburger_icon').addEventListener('click', function() {
+    const menu = document.getElementById('hamburger_menu');
+    menu.classList.toggle('active');
+});
+
+
+/*const hamburger_icon = document.querySelector('#hamburger_icon');
+const hamburger_menu = documet.querySelector('#hamburger_menu');
+
+function showhamburgermenu(){
+    document.getElementById("hamburger_menu").style.display = "block";
+}
+
+function nonehamburgermenu() {
+    document.getElementById("hamburger_menu").style.display ="none";
+}*/
+
+/*hamburger_icon.addEventListener('click', () => {
+    hamburger_menu.id('block');
+    });*/
+
+
+//hamburger_icon.addEventListener('click', showhamburgermenu);
+
